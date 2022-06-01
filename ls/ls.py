4232de144f479961d85b1dffcd2357943b7c253c
Hybrys <64049236+Hybrys@users.noreply.github.com>
2022-06-01 10:26:08 -0700

    Demystify the ANSI codes

--- ls/ls.py
+++ ls/ls.py
@@ -2,12 +2,16 @@ import os
 
 filelist = os.scandir(os.getcwd())
 
+# ANSI coding for simple colours
+BLUE = "\033[1;34m"
+END = "\033[0m"
+
 for file in filelist:
     filename = file.name
     
     if filename.find(' ') != -1:
         filename = f"'{filename}'"
     if file.is_dir():
-        filename = f"\033[1;34m {filename}/\033[0m"
+        filename = f"{BLUE}{filename}{END}/"
     print(filename)
 
